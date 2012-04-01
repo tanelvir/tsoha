8d50fda83a2f7d7258761da4e45d20c3fd11accf
Taneli <taneli@megaforce.(none)>
2012-04-01 22:14:55 +0300

    eka commit

---
+++ kirjautuminen.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Äänestys</p>'; ?> 
+ </body>
+<body>
+ <?php echo '<p>Ole hyvä ja syötä henkilötunnuksesi kirjautuaksesi sisään</p>'; ?> 
+ </body>
+</html>
