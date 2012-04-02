4a7300e2c6c109201e0e4137655c989026c766d6
Taneli <taneli@megaforce.(none)>
2012-04-02 14:10:57 +0300

    parempaa tunnusta

--- kirjautuminen.php
+++ kirjautuminen.php
@@ -3,14 +3,29 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Äänestys</p>'; ?> 
+<h3>Kirjautuminen</h3> 
  </body>
 <body>
  <?php echo '<p>Ole hyvä ja syötä henkilötunnuksesi kirjautuaksesi sisään</p>'; ?> 
  </body>
 <form action="kirjautuminen.php" method="post">
 <h3>Kirjautuminen</h3>
+<?php
+function valintalista($nimi, $sisalto) {
+    echo "<select name=\"{$nimi}\">";
+    foreach ($sisalto as $kohta) {
+        echo "<option value=\"{$kohta}\">{$kohta}";
+    }
+    echo "</select>";
+}
+
+echo "<p>Valitse syntymäpäiväsi:</p>";
+valintalista("paiva", range(1, 31));
+valintalista("kuukausi", range(1, 12));
+valintalista("vuosi", range(1900, 2010));
+?>
 <p>Tunnus: <br>
+echo "<p>Henkilötunnuksesi loppuosa:</p>";
 <input type="text" name="henkilöllisyystunnus"></p>
 <input type="submit" value="Kirjaudu">
 </form>
